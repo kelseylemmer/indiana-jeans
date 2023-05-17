61b60652b2ae0814f2c50c80f347fb22036033ac
Kelsey Lemmer <kelseylemmer@gmail.com>
2023-05-17 14:32:37 -0400

    added save submission button and created submission component

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
 import { OwnsJeansChoices } from "./OwnsJeans.js"
+import { SaveSubmission } from "./SaveSubmission.js"
 import { LocationTypeChoices } from "./UrbanDweller.js"
 
 
@@ -7,10 +8,13 @@ const container = document.querySelector("#container")
 const render = async () => {
     const jeanOwnershipHTML = OwnsJeansChoices()
     const locationsHTML = await LocationTypeChoices()
+    const buttonHTML = SaveSubmission()
 
     container.innerHTML = `
         ${jeanOwnershipHTML}
-        ${locationsHTML}`
+        ${locationsHTML}
+        ${buttonHTML}
+    `
 }
 
 render()
